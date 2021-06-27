ae9b2a03f1edcf89be3ad81128917132619d4ee4
Gianfranco <gfkeithgc@outlook.com>
2021-06-27 00:06:32 -0500

    8 Inlcuir nueva data

--- SQl/data.sql
+++ SQl/data.sql
@@ -18,20 +18,6 @@ USE `ieduca`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `__efmigrationshistory`
---
-
-DROP TABLE IF EXISTS `__efmigrationshistory`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `__efmigrationshistory` (
-  `MigrationId` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ProductVersion` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`MigrationId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `__efmigrationshistory`
 --
 
@@ -40,22 +26,6 @@ LOCK TABLES `__efmigrationshistory` WRITE;
 INSERT INTO `__efmigrationshistory` VALUES ('20210626151532_01-Init-Database','5.0.7');
 /*!40000 ALTER TABLE `__efmigrationshistory` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `albumes`
---
-
-DROP TABLE IF EXISTS `albumes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `albumes` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(220) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Artista` varchar(220) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Portada` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `albumes`
@@ -68,31 +38,12 @@ INSERT INTO `albumes` VALUES (1,'Allies','Keith Wallen','https://mir-s3-cdn-cf.b
 UNLOCK TABLES;
 
 --
--- Table structure for table `canciones`
---
-
-DROP TABLE IF EXISTS `canciones`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `canciones` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(220) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Duracion` time(6) NOT NULL,
-  `Enlace` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `AlbumId` int NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `IX_Canciones_AlbumId` (`AlbumId`),
-  CONSTRAINT `FK_Canciones_Albumes_AlbumId` FOREIGN KEY (`AlbumId`) REFERENCES `albumes` (`Id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `canciones`
 --
 
 LOCK TABLES `canciones` WRITE;
 /*!40000 ALTER TABLE `canciones` DISABLE KEYS */;
-INSERT INTO `canciones` VALUES (1,'Hey Baby','00:03:23.000000','https://www.youtube.com/watch?v=XDbq4rj7TjI&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(2,'Face in the light','00:03:23.000000','https://www.youtube.com/watch?v=xGwgtnHzqQA&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(3,'Hello houston','00:03:23.000000','https://www.youtube.com/watch?v=Bmi51mJyEn4&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(4,'Wicked game','00:03:23.000000','https://www.youtube.com/watch?v=WvFMx6JxDEU&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(5,'California Road','00:03:23.000000','https://www.youtube.com/watch?v=61n6t7YxCRE&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(6,'Failure','00:03:25.000000','sin',3),(7,'Dance with the devil','00:03:25.000000','sin',4);
+INSERT INTO `canciones` VALUES (1,'Hey Baby','00:03:23.000000','https://www.youtube.com/watch?v=XDbq4rj7TjI&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(2,'Face in the light','00:03:23.000000','https://www.youtube.com/watch?v=xGwgtnHzqQA&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(3,'Hello houston','00:03:23.000000','https://www.youtube.com/watch?v=Bmi51mJyEn4&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(4,'Wicked game','00:04:23.000000','https://www.youtube.com/watch?v=WvFMx6JxDEU&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(5,'California Road','00:02:22.000000','https://www.youtube.com/watch?v=61n6t7YxCRE&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',1),(6,'Failure','00:03:46.000000','https://www.youtube.com/watch?v=61n6t7YxCRE&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',3),(7,'Dance with the devil','00:04:25.000000','https://www.youtube.com/watch?v=61n6t7YxCRE&list=OLAK5uy_nBLH62igOk3SqnDV4ZgwjY6rAqzAsAT_c',4),(8,'Dream Away','00:03:14.000000','https://www.youtube.com/watch?v=ffgATC3-lU8',2),(9,'It´s find us all','00:04:15.000000','https://www.youtube.com/watch?v=ffgATC3-lU8',2),(10,'Warfare','00:02:50.000000','https://www.youtube.com/watch?v=ffgATC3-lU8',2);
 /*!40000 ALTER TABLE `canciones` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -105,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-26 16:33:22
+-- Dump completed on 2021-06-26 23:49:14
